bd35bc621fbca11ba537e1de23f99db11b744ec8
rickhanlonii <rickhanlonii@gmail.com>
2018-03-16 14:00:35 -0400

    Init

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (<div style={{display: 'flex', flexDirection: 'column' }}>
+    <div style={{flex: '1', textAlign: 'center' }}>wdym?</div>
+    <div style={{flex: '1', textAlign: 'center' }}>🚧🚧🚧</div>
+  </div>
+);
+
+export default Index;
